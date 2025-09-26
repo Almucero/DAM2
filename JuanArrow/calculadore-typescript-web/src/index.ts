0ebbadb11e52d72a310d6c2b606addd560cc93ec
Almucero <alvarokilor@gmail.com>
2025-09-26 14:43:04 +0200

    jail

--- JuanArrow/calculadore-typescript-web/src/index.ts
+++ JuanArrow/calculadore-typescript-web/src/index.ts
@@ -1,11 +1,1 @@
-// src/index.ts
-function greet(name: string): void {
-    const greeting = `Hello, ${name}!`;
-    const element = document.getElementById("greeting");
-    if (element) {
-      element.innerText = greeting;
-    }
-  }
-  
-  greet("World");
-  
+//añadir reloj: rxjs, innerhtml, interval, Date, getelementbyid
