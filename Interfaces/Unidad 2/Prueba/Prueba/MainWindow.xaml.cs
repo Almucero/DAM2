8696659ce4a5964f4cc2634dba7a9a806facb81f
Almucero <alvarokilor@gmail.com>
2025-11-12 10:03:49 +0100

    u

---
+++ Interfaces/Unidad 2/Prueba/Prueba/MainWindow.xaml.cs
@@ -0,0 +1,36 @@
+﻿using System.Windows;
+using System.Windows.Input;
+
+namespace Prueba
+{
+    /// <summary>
+    /// Lógica de interacción para MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Window_PreviewMouseDown(object sender, MouseButtonEventArgs e)
+        {
+            MessageBox.Show("Window");
+        }
+
+        private void StackPanel_PreviewMouseDown(object sender, MouseButtonEventArgs e)
+        {
+            MessageBox.Show("StackPanel");
+        }
+
+        private void Button_PreviewMouseDown(object sender, MouseButtonEventArgs e)
+        {
+            MessageBox.Show("Button");
+        }
+
+        private void Button_MouseEnter(object sender, MouseEventArgs e)
+        {
+            MessageBox.Show("Ratón");
+        }
+    }
+}
