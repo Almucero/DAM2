10f84fd49c3cbe06217609e59b9a0a12c9f4886f
Almucero <alvarokilor@gmail.com>
2025-11-20 11:58:28 +0100

    copiad ahi

--- Interfaces/Unidad 2/Prueba/Prueba/MainWindow.xaml.cs
+++ Interfaces/Unidad 2/Prueba/Prueba/MainWindow.xaml.cs
@@ -1,6 +1,4 @@
-﻿using System;
-using System.Windows;
-using System.Windows.Input;
+﻿using System.Windows;
 
 namespace Prueba
 {
@@ -9,14 +7,30 @@ namespace Prueba
     /// </summary>
     public partial class MainWindow : Window
     {
+        public int ColorDeOjos {
+            get { return (int)GetValue(ColorDeOjosProperty); }
+            set { SetValue(ColorDeOjosProperty, value);}
+        }
+        public static readonly DependencyProperty ColorDeOjosProperty = DependencyProperty.Register("ColorDeOjos", typeof(int), typeof(MainWindow), new PropertyMetadata(0));
+
+        public int TamañoTexto
+        {
+            get { return (int)GetValue(TamañoTextoProperty);  }
+            set { SetValue(TamañoTextoProperty, value);}
+        }
+        public static readonly DependencyProperty TamañoTextoProperty = DependencyProperty.Register("TamañoTexto", typeof(int), typeof(MainWindow), new PropertyMetadata(20));
+
         public MainWindow()
         {
             InitializeComponent();
+            MiSlider.Value = 30;
+            MiTextBox.Text = MiSlider.Value.ToString();
+            this.DataContext = this;
         }
 
-        private void No_Seleccionado(object sender, RoutedEventArgs e)
+        private void Button_Click(object sender, RoutedEventArgs e)
         {
-            MessageBox.Show("Esa no es la actitud");
+            TamañoTexto += 2;
         }
     }
 }
