b3e90f857e7223a4bb15d3e35fd78267074540b7
Almucero <alvarokilor@gmail.com>
2026-01-13 14:25:45 +0100

    16/17

---
+++ Hormigo/CameraDemo/app/src/main/java/com/example/camerademo/CameraViewModel.kt
@@ -0,0 +1,95 @@
+package com.example.camerademo
+
+import android.content.ContentValues
+import android.content.Context
+import android.provider.MediaStore
+import android.widget.Toast
+import androidx.camera.core.CameraSelector
+import androidx.camera.core.CameraSelector.DEFAULT_BACK_CAMERA
+import androidx.camera.core.CameraSelector.DEFAULT_FRONT_CAMERA
+import androidx.camera.core.ImageCapture
+import androidx.camera.core.ImageCaptureException
+import androidx.camera.core.Preview
+import androidx.camera.core.SurfaceRequest
+import androidx.camera.lifecycle.ProcessCameraProvider
+import androidx.camera.lifecycle.awaitInstance
+import androidx.compose.foundation.Image
+import androidx.core.content.ContextCompat
+import androidx.lifecycle.LifecycleOwner
+import androidx.lifecycle.ViewModel
+import kotlinx.coroutines.awaitCancellation
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.asStateFlow
+
+class CameraViewModel(
+
+): ViewModel() {
+    private val _surfaceRequest = MutableStateFlow<SurfaceRequest?>(null)
+    val surfaceRequest: StateFlow<SurfaceRequest?>
+        get() = _surfaceRequest.asStateFlow()
+
+    private val _cameraSelector = MutableStateFlow(DEFAULT_BACK_CAMERA)
+    val cameraSelector: StateFlow<CameraSelector>
+        get() = _cameraSelector.asStateFlow()
+
+    private val imageCapture = ImageCapture.Builder().build()
+
+    private val cameraPreviewUseCase = Preview.Builder().build().apply {
+        setSurfaceProvider {
+            newSurfaceRequest -> _surfaceRequest.value = newSurfaceRequest
+        }
+    }
+
+    fun takePicture(context: Context) {
+        val contentValues = ContentValues().apply {
+            put(MediaStore.MediaColumns.DISPLAY_NAME, "img_${System.currentTimeMillis()}" )
+            put(MediaStore.MediaColumns.MIME_TYPE, "image/jpeg")
+        }
+        val outputOptions = ImageCapture.OutputFileOptions.Builder(
+            context.contentResolver,
+            MediaStore.Images.Media.EXTERNAL_CONTENT_URI,
+            contentValues
+        ).build()
+
+        imageCapture.takePicture(
+            outputOptions,
+            ContextCompat.getMainExecutor(context),
+            object : ImageCapture.OnImageSavedCallback {
+                override fun onImageSaved(output: ImageCapture.OutputFileResults) {
+                    Toast.makeText(context, "Foto Guardada", Toast.LENGTH_SHORT).show()
+                }
+                override fun onError(exc: ImageCaptureException) {
+                    Toast.makeText(context, "Error: ${exc.message}", Toast.LENGTH_SHORT).show()
+                }
+            }
+        )
+    }
+
+    fun switchCamera() {
+        _cameraSelector.value = if (_cameraSelector.value == DEFAULT_BACK_CAMERA) {
+            DEFAULT_FRONT_CAMERA
+        } else {
+            DEFAULT_BACK_CAMERA
+        }
+    }
+
+    suspend fun bindToCamera(
+        context: Context,
+        lifecycleOwner: LifecycleOwner,
+        cameraSelector: CameraSelector
+    ) {
+        val processCameraProvider = ProcessCameraProvider.awaitInstance(context)
+        processCameraProvider.bindToLifecycle(
+            lifecycleOwner = lifecycleOwner,
+            cameraSelector,
+            cameraPreviewUseCase,
+            imageCapture
+        )
+        try {
+            awaitCancellation()
+        } finally {
+            processCameraProvider.unbindAll()
+        }
+    }
+}
